82b6879af5e7bbcc6a99ca891699d781f595ed1b
jclaroee <41116919+jclaroee@users.noreply.github.com>
2021-10-11 19:57:58 -0400

    Update BST.py
    Complete

--- BST.py
+++ BST.py
@@ -1,49 +1,80 @@
-#BST wrapper handles management of node classes 
+# BST wrapper handles management of node classes 
 
 class node:
     def __init__(self,value=None):
-        self.value=value
-        self.left_child=None                # left child pointer
-        self.right_child=None               # right child pointer
+        self.value = value
+        self.left_child = None                # left child pointer
+        self.right_child = None               # right child pointer
 
 class binary_search_tree:
     def __init__(self):
-      self.root=None                        # root null at init
+        self.root = None                        # root null at init
 
-#Insert node function
+# Insert node function
 
-def insert(self,value):
-    if self.root==None:
-        self.root=node(value)               # root of BST
-    else:
-        self._insert(value,self.root)       # insert node
-
-def _insert(self, value, cur_node):         
-    if value < cur_node.value:               # left child ?
-        if cur_node.left_child==None:
-            cur_node.left_child=node(value) 
+    def insert(self,value):
+        if self.root == None:
+            self.root = node(value)               # root of BST
         else:
-            self._insert(value,cur_node.left_child)   # insert new left child
-    elif value > cur_node.value:
-        if cur_node.right_child==None:
-            cur_node.right_child=node(value)
-        else:
-            self._insert(value,cur_node.right_child)    # insert new right child
-    else:
-        print ("Value already in tree!")
+            self._insert(value,self.root)         # insert node
 
-def print_tree(self):                       # what is in the tree
-    if self.root!=None:
-        self.print_tree(self.root)
+    def _insert(self, value, cur_node):         
+         if value < cur_node.value:               # left child ?
+             if cur_node.left_child == None:
+                 cur_node.left_child = node(value) 
+             else:
+                 self._insert(value,cur_node.left_child)   # insert new left child
+         elif value > cur_node.value:
+             if cur_node.right_child == None:
+                 cur_node.right_child=node(value)
+             else:
+                 self._insert(value,cur_node.right_child)    # insert new right child
+         #else:
+             #print ("Value already in tree!")
 
-def _print_tree(self,cur_node):
-    if cur_node!=None:
-        self._print_tree(cur_node.left_child)
-        print (str(cur_node.value))
-        self.print_tree(cur_node.right_child)
+    def print_tree(self):                       # what is in the tree
+        if self.root != None:
+             print ("Input:")
+             self._print_tree(self.root)
+
+    def _print_tree(self,cur_node):
+         if cur_node!=None:
+             self._print_tree(cur_node.left_child)
+             print (str(cur_node.value))
+             self._print_tree(cur_node.right_child)
+
+    def height(self):
+         if self.root != None:
+             return self._height(self.root,0)
+         else:
+            return 0
+
+    def _height(self,cur_node,cur_height):
+        if cur_node==None: return cur_height                            # base case
+
+        left_height = self._height(cur_node.left_child,cur_height+1)
+        right_height = self._height(cur_node.right_child,cur_height+1)
+        return max(left_height,right_height)
+
+    def deepest_Node(self,cur_depth):
+         if self.root != None:
+             return self._deepest_Node(self.root,cur_depth)
+         else:
+            return 0 
+
+    def _deepest_Node(self,cur_node,cur_depth):
+         if cur_node==None: return cur_depth
+
+         if (cur_depth == 1):
+                 print("deepest: ", cur_node.value)
+                 
+         elif (cur_depth > 1):
+             left_depth = self._deepest_Node(cur_node.left_child,cur_depth - 1)
+             right_depth = self._deepest_Node(cur_node.right_child,cur_depth - 1)
+             
 
 
-def fill_tree(tree,num_elems=100,max_int=1000):
+def fill_tree(tree,num_elems = 40,max_int = 100):
     from random import randint
     for _ in range(num_elems):
         cur_elem = randint(0,max_int)
@@ -52,5 +83,10 @@ def fill_tree(tree,num_elems=100,max_int=1000):
 
 tree = binary_search_tree()
 tree = fill_tree(tree)
-
 tree.print_tree()
+
+cur_depth = tree.height()
+dn = tree.deepest_Node(cur_depth)
+print ("At depth: "+str(cur_depth))
+
+#print ("tree height: "+str(tree.height()))
